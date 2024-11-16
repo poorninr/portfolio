3996001c0523faf691e255b677a6681592a9f143
poorninr <116303124+poorninr@users.noreply.github.com>
2024-11-16 22:13:25 +0530

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,13 @@
+menuBar=document.getElementById("menu-bar-icon");
+menuBarClose=document.getElementById("menu-bar-close-icon");
+menuBarContainer=document.getElementById("menu-bar-container");
+
+
+function update(){
+    menuBarContainer.style.left="0"
+}
+
+menuBarClose.addEventListener("click",reset)
+function reset(){
+    menuBarContainer.style.left="-100%"
+}
