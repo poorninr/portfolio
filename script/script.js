f262cd560b3ffa2fcea18e3dd6e66d5100499e39
Poorni <poornimapnr5@gmail.com>
2024-12-12 22:36:03 +0530

    about page changes

--- script/script.js
+++ script/script.js
@@ -22,8 +22,26 @@ for(i=0; i<listItems.length;i++)
 }
 
 
-contactInputs=document.querySelector('#contact-form-container')
-contactBtn=document.querySelector('#contact-button')
+let smallPara=document.getElementById("para1-container")
+let bigPara=document.getElementById("para3-container")
+let readMoreBtn=document.getElementById("about-para-btn1")
+let readLessBtn=document.getElementById("about-para-btn2")
+
+
+readMoreBtn.addEventListener('click',readmore)
+function readmore(){
+    smallPara.style.display="none"
+    bigPara.style.display="block"
+}
+
+readLessBtn.addEventListener('click',readless)
+function readless(){
+    bigPara.style.display="none"
+    smallPara.style.display="block"
+}
+
+// contactInputs=document.querySelector('#contact-form-container')
+// contactBtn=document.querySelector('#contact-button')
 
 // contactBtn.addEventListener("click",submitForm)
 // function submitForm() {
